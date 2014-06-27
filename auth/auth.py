d1b6ceca9cc5185cc66a9a035920d93765952f33
Duane Johnson <duane.johnson@gmail.com>
2014-06-26 20:09:28 -0600

    Update auth to redirect unauthorized ajax reqs to login

--- auth/auth.py
+++ auth/auth.py
@@ -1,15 +1,19 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, redirect, render_template_string, request, url_for
+from flask import Flask, redirect, render_template_string, request, url_for, send_from_directory, jsonify
 from flask.ext.babel import Babel
 from flask.ext.mail import Mail
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.user import current_user, login_required, UserManager, UserMixin, SQLAlchemyAdapter
+from flask.ext.login import login_url
+from functools import wraps
 import datetime
 import jwt
+import os
 
 # Use a Class-based config to avoid needing a 2nd file
 class ConfigClass(object):
     CSRF_ENABLED = True
+    CONSUMER_TTL = 86400
 
     # Configure Flask-User
     USER_ENABLE_USERNAME         = True
@@ -43,6 +47,28 @@ def create_app(test_config=None):                   # For automated tests
         translations = [str(translation) for translation in babel.list_translations()]
         return request.accept_languages.best_match(translations)
 
+    # JSON Web Token generator
+    def generate_token(user_id):
+        return jwt.encode({
+          'consumerKey': app.config['CONSUMER_KEY'],
+          'userId': user_id,
+          'issuedAt': _now().isoformat() + 'Z',
+          'ttl': app.config['CONSUMER_TTL']
+        }, app.config['CONSUMER_SECRET'])
+
+    def ajax_login_required(func):
+        @wraps(func)
+        def decorated_view(*args, **kwargs):
+            if app.login_manager._login_disabled:
+                return func(*args, **kwargs)
+            elif not current_user.is_authenticated():
+                return jsonify({'redirect': login_url(url_for('user.login'), request.url)})
+            return func(*args, **kwargs)
+        return decorated_view
+
+    def _now():
+        return datetime.datetime.utcnow().replace(microsecond=0)
+
     # Define User model. Make sure to add flask.ext.user UserMixin!!
     class User(db.Model, UserMixin):
         id = db.Column(db.Integer, primary_key=True)
@@ -62,44 +88,47 @@ def create_app(test_config=None):                   # For automated tests
     user_manager = UserManager(db_adapter, app)     # Init Flask-User and bind to app
 
     # Display Login page or Profile page
-    @app.route('/')
-    def home_page():
-        if current_user.is_authenticated():
-            return redirect(url_for('profile_page'))
-        else:
-            return redirect(url_for('user.login'))
+    # @app.route('/')
+    # def home_page():
+    #     if current_user.is_authenticated():
+    #         return redirect(url_for('profile_page'))
+    #     else:
+    #         return redirect(url_for('user.login'))
 
     # The '/profile' page requires a logged-in user
-    @app.route('/profile')
+    @app.route('/user/profile')
     @login_required
     def profile_page():
         return render_template_string("""
             {% extends "base.html" %}
             {% block content %}
-                <h2>{%trans%}Profile Page{%endtrans%}</h2>
-                <p> {%trans%}Hello{%endtrans%}
+                <h2>{% trans %}Profile Page{% endtrans %}</h2>
+                <p> {% trans %}Hello{% endtrans %}
                     {{ current_user.username or current_user.email }},</p>
                 <p> <a href="{{ url_for('user.change_username') }}">
-                    {%trans%}Change username{%endtrans%}</a></p>
+                    {% trans %}Change username{% endtrans %}</a></p>
                 <p> <a href="{{ url_for('user.change_password') }}">
-                    {%trans%}Change password{%endtrans%}</a></p>
+                    {% trans %}Change password{% endtrans %}</a></p>
                 <p> <a href="{{ url_for('user.logout') }}?next={{ url_for('user.login') }}">
-                    {%trans%}Sign out{%endtrans%}</a></p>
+                    {% trans %}Sign out{% endtrans %}</a></p>
             {% endblock %}
             """)
 
+    # Generate a token for AnnotateIt authentication
+    @app.route('/user/token')
+    @ajax_login_required
+    def get_token():
+        return jsonify({'token': generate_token(current_user.id)})
+
+    @app.route('/')
+    def send_root():
+        return send_from_directory(os.path.join(app.root_path, '..', 'build'), 'index.html')
+
+    @app.route('/<path:filename>')
+    def send_static(filename):
+        return send_from_directory(os.path.join(app.root_path, '..', 'build'), filename)
+
     return app
-
-    def generate_token(user_id):
-        return jwt.encode({
-          'consumerKey': CONSUMER_KEY,
-          'userId': user_id,
-          'issuedAt': _now().isoformat() + 'Z',
-          'ttl': CONSUMER_TTL
-        }, CONSUMER_SECRET)
-
-    def _now():
-        return datetime.datetime.utcnow().replace(microsecond=0)
 
 # Start development web server
 if __name__=='__main__':
